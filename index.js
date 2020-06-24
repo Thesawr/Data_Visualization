c89685958e16b8ab959f27f3485c5e9616dee5ac
SAWR <birgun_sarun@hotmail.com>
2020-06-24 12:34:02 -0500

    Add file to repo
    Add file to repo

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+function square(number){
+    return number*number;
+}
+
+let number = square(2);
+console.log(number);
